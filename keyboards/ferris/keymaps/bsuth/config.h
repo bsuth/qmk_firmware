066a49b7dab1cca65dc9ca5cab9a358310d0cac0
bsuth <bsuth701@gmail.com>
2023-11-26 17:45:24 +0900

    add ferris keymap

---
+++ keyboards/ferris/keymaps/bsuth/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define MASTER_LEFT
+#undef MASTER_RIGHT
+#undef EE_HANDS
+
+// -----------------------------------------------------------------------------
+// Tap Hold Configuration
+//
+// https://docs.qmk.fm/#/tap_hold
+// -----------------------------------------------------------------------------
+
+#define PERMISSIVE_HOLD
